d7ee69049850017fb26f68db59e036dce01e88d9
Melanie Brown <brown.m@pm.me>
2023-12-19 14:47:35 -0500

    MonadInference

--- lib/Language/SystemDE.hs
+++ lib/Language/SystemDE.hs
@@ -1,7 +1,8 @@
 module Language.SystemDE where
 
-import Control.Applicative (Alternative (empty))
-import Control.Monad (MonadPlus, guard, void)
+import Control.Monad (MonadPlus, unless, void, when)
+import Control.Monad.Except (ExceptT)
+import Control.Monad.Trans.Except (throwE)
 import Data.Functor (($>), (<&>))
 import Data.Text (Text)
 
@@ -32,18 +33,6 @@ data Context
   | (:&) Context (Var Point)
   deriving (Eq, Ord, Show)
 
--- read `Var x d0 aA <- name ∈ context` as `context |- x ::d0 aA`
-(∈) :: (Alternative m) => Text -> Context -> m (Var Point)
-name ∈ context0 = case context0 of
-  C_ -> empty
-  _ :& v@(Var (Id x) _ _) | x == name -> pure v
-  c :& _ -> name ∈ c
-
-(∉) :: (Alternative m) => Text -> Context -> m ()
-(∉) x = \case
-  C_ -> pure ()
-  c :& Var (Id y) _ _ -> guard (x /= y) *> x ∉ c
-
 resurrect :: Context -> Context
 resurrect = \case
   C_ -> C_
@@ -51,19 +40,6 @@ resurrect = \case
 
 (~) :: Context -> Context
 (~) = resurrect
-
-(+:) :: (MonadPlus m) => Context -> Var Point -> m Context
-c +: (Var (Id x) (Mode r t) a) = do
-  -- T-ConsTm
-  infer (c ~) t a .= Star
-  x ∉ c
-  --
-  pure (c :& Var (Id x) (Mode r t) a)
-
-infixl 7 .=
-
-(.=) :: (Eq x, MonadPlus m) => m x -> x -> m ()
-action .= x = guard . (x ==) =<< action
 
 infixl 9 :>
 
@@ -225,6 +201,103 @@ recoerce coercion with name = case coercion of
     do (y, recoerce g3 with name)
     do recoerce g4 with name
 
+value :: Point -> Bool
+value = \case
+  Star{} -> True
+  Point{} -> True
+  Pi{} -> True
+  Abs{} -> True
+  Equal{} -> True
+  Reify{} -> True
+  NN{} -> True
+  Zero{} -> True
+  Succ{} -> True
+  _ -> False
+
+covalue :: Point -> Bool
+covalue = \case
+  c :> _ -> covalue c
+  Succ c -> covalue c
+  p -> value p
+
+data InferenceError
+  = TypeOfStarLogical
+  | NeedsStrongerTermination Termination Termination
+  | NeedsStrongerRelevance Relevance Relevance
+  | Mismatch Point Point
+  | MismatchPath Path Path
+  | UnknownIdentifier Id
+  | ReusedIdentifier Id (Var Point)
+  | ApplicationPatternFailure Point Point
+  | InductionPatternFailure Point
+  | ApplicationPathPatternFailure Path
+  | ReflectionPatternFailure Point
+  | InductionCongruencePatternFailure Coercion
+  | UtterFailure
+instance Semigroup InferenceError where
+  (<>) :: InferenceError -> InferenceError -> InferenceError
+  (<>) = const
+instance Monoid InferenceError where
+  mempty :: InferenceError
+  mempty = UtterFailure
+
+class (MonadPlus m) => MonadInference m where
+  inferenceError :: InferenceError -> m a
+
+instance MonadInference Maybe where
+  inferenceError :: InferenceError -> Maybe a
+  inferenceError = const Nothing
+
+instance (Monad m) => MonadInference (ExceptT InferenceError m) where
+  inferenceError :: InferenceError -> ExceptT InferenceError m a
+  inferenceError = throwE
+
+-- read `Var x d0 aA <- name ∈ context` as `context |- x ::d0 aA`
+(∈) :: (MonadInference m) => Id -> Context -> m (Var Point)
+name ∈ context0 = case context0 of
+  C_ -> inferenceError do UnknownIdentifier name
+  _ :& v@(Var x _ _) | x == name -> pure v
+  c :& _ -> name ∈ c
+
+(∉) :: (MonadInference m) => Id -> Context -> m ()
+(∉) x = \case
+  C_ -> pure ()
+  c :& p@(Var y _ _) -> do
+    when (x == y) do inferenceError do ReusedIdentifier x p
+    x ∉ c
+
+(+:) :: (MonadInference m) => Context -> Var Point -> m Context
+c +: (Var x (Mode r t) a) = do
+  -- T-ConsTm
+  infer (c ~) t a .= Star
+  x ∉ c
+  --
+  pure (c :& Var x (Mode r t) a)
+
+infixl 7 .=
+infixl 7 .==
+
+(.=) :: (MonadInference m) => m Point -> Point -> m ()
+action .= x = do
+  y <- action
+  mismatchUnequal x y
+
+(.==) :: (MonadInference m) => m Path -> Path -> m ()
+action .== x = do
+  y <- action
+  unless (x == y) do inferenceError do MismatchPath x y
+
+terminate :: (MonadInference m) => Termination -> Termination -> m ()
+terminate t0 t =
+  unless (t0 <= t) do inferenceError do NeedsStrongerTermination t0 t
+
+relieve :: (MonadInference f) => Relevance -> Relevance -> f ()
+relieve r0 r =
+  unless (r0 <= r) do inferenceError do NeedsStrongerRelevance r0 r
+
+mismatchUnequal :: (MonadInference f) => Point -> Point -> f ()
+mismatchUnequal a b = unless (a == b) do inferenceError do Mismatch a b
+
 -- |
 -- Type inference algorithm for System DE.
 --
@@ -233,17 +306,18 @@ recoerce coercion with name = case coercion of
 --
 -- >>> infer @Maybe C_ Program Star
 -- Just Star
-infer :: (MonadPlus m) => Context -> Termination -> Point -> m Point
+infer :: (MonadInference m) => Context -> Termination -> Point -> m Point
 infer c t = \case
   -- read `ty <- point` as `point :: ty`
   Star -> case t of
     -- T-TYPE
-    Logical -> empty
+    Logical -> inferenceError TypeOfStarLogical
     Program -> pure Star
   Point x -> do
     -- T-Var
-    Var _ rt0 a <- x ∈ c
-    guard (rt0 <= Mode Relevant t)
+    Var _ rt0 a <- Id x ∈ c
+    terminate rt0.theta t
+    relieve rt0.rho Relevant
     --
     pure a
   Pi (Var x (Mode _ t0) aA) b -> do
@@ -265,11 +339,12 @@ infer c t = \case
     tyB <- infer c t b
     case tyB of
       Pi (Var (Id x) m aA) bB -> do
-        guard (m <= mode)
+        terminate m.theta mode.theta
+        relieve m.rho mode.rho
         infer c t a .= aA
         --
         pure (repoint bB a x)
-      _ -> empty
+      _ -> inferenceError do ApplicationPatternFailure b a
   Equal t0 a b -> do
     -- T-Eq
     aA <- infer c t0 a
@@ -306,7 +381,7 @@ infer c t = \case
     infer c' Logical a3 .= Pi (x ^: ayx) asyx
     --
     pure (repoint aA a1 x)
-  Ind{} -> empty
+  p@Ind{} -> inferenceError do InductionPatternFailure p
 
 infixr 8 :==:
 
@@ -315,23 +390,24 @@ data Path = Point :==: Point
 
 -- |
 -- Prove the equality of two 'Point's along a 'Path' given a 'Coercion'.
-prove :: (MonadPlus m) => Context -> Termination -> Coercion -> m Path
+prove :: (MonadInference m) => Context -> Termination -> Coercion -> m Path
 prove c t = \case
   Reflect a0@(Equal t0 a b) -> do
     -- E-Reflect
-    guard (t0 <= t)
+    terminate t0 t
     void (infer c Logical a0)
     aA <- infer c t a
     infer c t b .= aA
     --
     pure (a :==: b)
+  Reflect p -> inferenceError do ReflectionPatternFailure p
   Reflex a -> infer c t a $> a :==: a -- E-Reflex
   Sym g -> prove c t g <&> \(a :==: b) -> b :==: a -- E-Sym
   g1 ::: g2 -> do
     -- E-Trans
     x :==: b1 <- prove c t g1
     b2 :==: y <- prove c t g2
-    guard (b1 == b2)
+    mismatchUnequal b1 b2
     --
     pure (x :==: y)
   PiCong (Var (Id x) (Mode r0 t0) g1) g2 -> do
@@ -381,28 +457,28 @@ prove c t = \case
   ReifyCong t0 g1 g2 -> do
     -- E-ReifyCong
     p <- prove c t0 g1
-    prove c t0 g2 .= p
+    prove c t0 g2 .== p
     --
     pure (Reify t0 g1 :==: Reify t0 g2)
   Pifst t0 g -> do
     -- E-PiFst
     prove c t0 g >>= \case
       Pi (Var _ (Mode _ th) a1) _ :==: Pi (Var _ _ a2) _ -> do
-        guard (th <= t)
+        terminate th t
         --
         pure (a1 :==: a2)
-      _ -> empty
+      p -> inferenceError do ApplicationPathPatternFailure p
   Pisnd t0 g g1 g2 -> do
     -- E-PiSnd
-    guard (t0 <= t)
+    terminate t0 t
     prove c t0 g >>= \case
       Pi (Var (Id x1) (Mode _ t1) aA1) bB1 :==: Pi (Var (Id x2) _ aA2) bB2 -> do
         a1 :==: a2 <- prove c t1 g1
         infer c t1 a2 .= aA2
-        prove c t1 g2 .= aA2 :==: aA1
+        prove c t1 g2 .== aA2 :==: aA1
         --
         pure (repoint bB1 (a1 :> g2) x1 :==: repoint bB2 a2 x2)
-      _ -> empty
+      p -> inferenceError do ApplicationPathPatternFailure p
   ConvCong g g1 g2 -> do
     -- E-ConvCong
     a1 :==: a2 <- prove c t g
@@ -432,70 +508,50 @@ prove c t = \case
     a3 :==: b3 <- prove c' Logical g3
     a0 <- infer c t (IndN x aA a1 a2 y a3)
     b0 <- infer c t (IndN x aA b1 b2 y b3)
-    prove c t g .= a0 :==: b0
+    prove c t g .== a0 :==: b0
     --
     pure (IndN x aA a1 a2 y a3 :> g :==: IndN x aA b1 b2 y b3)
-  _ -> empty
+  g@IndCong{} -> inferenceError do InductionCongruencePatternFailure g
 
-reducePrim :: (MonadPlus m) => Context -> Termination -> Point -> m Point
+reducePrim :: (MonadInference m) => Context -> Termination -> Point -> m Point
 reducePrim c t = liftA2 (*>) (infer c t) \case
   -- aBeta-AppAbs
   App mode (Abs (Var (Id x) m _) a) b | mode == m -> pure (repoint a b x)
   Abs l@(Var (Id x) (Mode _ t0) _) a1 :> g -> do
     -- aBeta-AbsPush
-    guard (t0 <= t) <* infer c t (Abs l a1 :> g)
+    void (infer c t (Abs l a1 :> g))
+    terminate t0 t
     prove c t0 g >>= \case
       Pi _ _ :==: Pi (Var (Id y) d2 aA2) _ -> do
         let a2 = repoint a1 (Point x :> Sym (Pifst t0 g)) x
         let g2 = Pisnd t0 g (Reflex (Point x)) (Sym (Pifst t0 g))
         --
         pure (Abs (Var (Id y) d2 aA2) (a2 :> g2))
-      _ -> empty
+      p -> inferenceError do ApplicationPathPatternFailure p
   a :> g1 :> g2 -> pure (a :> (g1 ::: g2)) -- aBeta-Combine
   a :> g -> do
     -- aBeta-ConvRefl
     aA1 :==: aA2 <- prove c t g
-    guard (aA1 == aA2)
+    mismatchUnequal aA1 aA2
     --
     pure a
   IndN _ _ Zero a2 _ _ -> pure a2 -- aBeta-IndZero
   IndN x aA (Succ a1) a2 y a3 ->
     -- aBeta-IndSucc
     pure (App RL (repoint a3 a1 y) (IndN x aA a1 a2 y a3))
-  _ -> empty
-
-value :: (MonadPlus m) => Point -> m ()
-value = \case
-  Star -> pure ()
-  Point _ -> pure ()
-  Pi _ _ -> pure ()
-  Abs _ _ -> pure ()
-  Equal{} -> pure ()
-  Reify _ _ -> pure ()
-  NN -> pure ()
-  Zero -> pure ()
-  Succ _ -> pure ()
-  _ -> empty
-
-covalue :: (MonadPlus m) => Point -> m ()
-covalue = \case
-  c :> _ -> covalue c
-  Succ c -> covalue c
-  p -> value p
+  p -> pure p
 
 -- | Computational
 --
 -- > _ :: Γ |- a ~> b
-reduceC1 :: (MonadPlus m) => Context -> Termination -> Point -> m Point
+reduceC1 :: (MonadInference m) => Context -> Termination -> Point -> m Point
 reduceC1 c t = \case
   App d0 a b
-    -- this is really fishy. it should probably be @m here.
-    -- that or 'value' and 'covalue' should be Point -> Bool
-    | Just () <- covalue @Maybe a -> do
+    | covalue a -> do
         -- R-AppAbs
         reduceA1 c t a >>= \case
           Abs (Var (Id x) _ _) a1 -> pure (repoint a1 b x)
-          _ -> empty
+          _ -> inferenceError do ApplicationPatternFailure a b
     | otherwise -> do
         -- R-App
         a2 <- reduceC1 c t a
@@ -513,18 +569,18 @@ reduceC1 c t = \case
     let g = Reflex bB1 :: Coercion
     -- because reduction is confluent and preserves
     -- Reflex bB0 : bB0 :==: bB0 ~> Reflex bB1 : bB1 :==: bB1.
-    prove (c ~) t g .= bB1 :==: bB0
+    prove (c ~) t g .== bB1 :==: bB0
     pure (IndN x aA b1 a2 y a3 :> g)
   p -> pure p
 
 -- | Administrative reduction
 --
 -- > _ :: Γ |- a ⇀ b
-reduceA1 :: (MonadPlus m) => Context -> Termination -> Point -> m Point
+reduceA1 :: (MonadInference m) => Context -> Termination -> Point -> m Point
 reduceA1 c t = \case
   l@(Abs (Var (Id x) (Mode r0 t0) _) a1 :> g) -> do
     -- CR-AbsPush
-    guard (t0 <= t)
+    terminate t0 t
     void (infer c t l) -- .= A
     prove (c ~) t0 g >>= \case
       Pi _ _ :==: Pi (Var _ _ aA2) _ -> do
@@ -532,7 +588,7 @@ reduceA1 c t = \case
             g2 = Pisnd t0 g (Reflex (Point x)) (Sym (Pifst t0 g))
         --
         pure (Abs (Var (Id x) (Mode r0 t0) aA2) a2 :> g2)
-      _ -> empty
+      p -> inferenceError do ApplicationPathPatternFailure p
   a :> g1 :> g2 -> pure (a :> (g1 ::: g2)) -- CR-Combine
   a :> g
     -- CR-ConvRefl
